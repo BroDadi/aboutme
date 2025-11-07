17457b1695f451def07ce27993922283be5943ea
BroDadi <brodadi123@gmail.com>
2025-11-07 19:37:37 +0600

    some more changes to make the page less cringy

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 window.onload = function()
 {
     let today = new Date();
-
-    let isAfterMarch14th;
-    if (today.getMonth() > 2 || (today.getMonth() == 2 && today.getDate() > 14))
-    {
-        isAfterMarch14th = true;
-    }
-
     let year = today.getFullYear();
-    if (isAfterMarch14th) year++;
+    let age = year - 2009;
+    if (today < new Date(year, 2, 14)) age--;
 
-    let nextMarch14th = new Date(year, 2, 14);
-    let daysToBday = Math.ceil((nextMarch14th.getTime() - today.getTime()) / 1000 / 86400);
-
-    let age = today.getFullYear() - 2009;
+    let nextMarch14th = new Date(today < new Date(year, 2, 15) ? year : year + 1, 2, 14);
+    let daysToBday = Math.ceil((nextMarch14th - today) / 86400000);
     
     if (daysToBday <= 0)
     {
         document.querySelector("#daycount").innerText = "(TODAY!!!)";
         document.querySelector("#daycount").classList.add("bdaytoday");
-        age++;
     }
     else
     {
-        document.querySelector("#daycount").innerText = "(in exactly " + daysToBday + " days!)";
+        document.querySelector("#daycount").innerText = `(in exactly ${daysToBday} days!)`;
     }
-
-    if (!isAfterMarch14th) age--;
     document.querySelector("#age").innerText = age;
 }
